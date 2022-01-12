784e319e74dc6a5fc2de78a55a97c9c207397415
mradamgbeck <mradamgbeck@gmail.com>
2022-01-12 10:37:50 -0500

    make esteemer access the same spek dataset as think pudding

--- bin/esteemer.sh
+++ bin/esteemer.sh
@@ -10,38 +10,54 @@ Usage:
 Esteemer reads a spek from stdin or provided file path.  
 
 Options:
-  -h | --help   print help and exit
-  -s | --spek   path to spek file (default to stdin)
+  -h | --help     print help and exit
+  -p | --pathways path to causal pathways
+  -s | --spek     path to spek file (default to stdin)
 HEREDOC
+
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+    LC_COLLATE=$old_lc_collate
+}
 
 # Parse args
 PARAMS=()
-while (( "$#" )); do
+while (("$#")); do
   case "$1" in
-    -h|--help)
-      echo "${USE_MSG}"
-      exit 0
-      ;;
-    -p|--pathways)
-      CP_FILE="${2}"
-      shift 2
-      ;;
-    -s|--spek)
-      SPEK_FILE="${2}"
-      shift 2
-      ;;
-    --) # end argument parsing
-      shift
-      break
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Aborting: Unsupported flag $1" >&2
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS+=("${1}")
-      shift
-      ;;
+  -h | --help)
+    echo "${USE_MSG}"
+    exit 0
+    ;;
+  -p | --pathways)
+    CP_FILE="${2}"
+    shift 2
+    ;;
+  -s | --spek)
+    SPEK_FILE="${2}"
+    shift 2
+    ;;
+  --) # end argument parsing
+    shift
+    break
+    ;;
+  -* | --*=) # unsupported flags
+    echo "Aborting: Unsupported flag $1" >&2
+    exit 1
+    ;;
+  *) # preserve positional arguments
+    PARAMS+=("${1}")
+    shift
+    ;;
   esac
 done
 
@@ -49,13 +65,13 @@ done
 FUSEKI_PING=$(curl -s -o /dev/null -w "%{http_code}" localhost:3030/$/ping)
 if [[ -z ${FUSEKI_PING}} || ${FUSEKI_PING} -ne 200 ]]; then
   # Error
-  echo >&2 "Fuseki not running locally."; 
+  echo >&2 "Fuseki not running locally."
 
   # Try to start custom fuseki locally
-  FUSEKI_DIR=/opt/fuseki/apache-jena-fuseki-3.10.0
-  ${FUSEKI_DIR}/fuseki-server --mem --update /ds 1> fuseki.out 2>&1 &
+  #  FUSEKI_DIR=/opt/fuseki/apache-jena-fuseki-3.10.0
+  ${FUSEKI_HOME}/fuseki-server --mem --update /ds 1>fuseki.out 2>&1 &
 
-  exit 1;
+  exit 1
 fi
 
 # Define SPARQL Queries for updates and results
@@ -72,11 +88,10 @@ INSERT {
 }
 USING <http://localhost:3030/ds/spek>
 WHERE {
-  ?candi a cpo:cpo_0000053 .
+  ?candi a obo:cpo_0000053 .
   ?candi slowmo:acceptable_by ?o
 }
 SPARQL
-
 
 # Read from SPEK_FILE or pipe from stdin
 #   Use '-' to instruct curl to read from stdin
@@ -85,16 +100,19 @@ if [[ -z ${SPEK_FILE} ]]; then
 fi
 
 # Load spek into fuseki
+FUSEKI_DATASET_URL="http://localhost:3030/ds"
+SPEK_URL="${FUSEKI_DATASET_URL}/spek"
+ENCODED_SPEK_URL=$(urlencode "${SPEK_URL}")
 curl --silent -X PUT --data-binary "@${SPEK_FILE}" \
   --header 'Content-type: application/ld+json' \
-  'http://localhost:3030/ds?graph=spek' >&2
+  "${FUSEKI_DATASET_URL}?graph=${ENCODED_SPEK_URL}" >&2
 
 # run update sparql
 curl --silent -X POST --data-binary "${UPD_SPARQL}" \
   --header 'Content-type: application/sparql-update' \
-  'http://localhost:3030/ds/update' >&2
+  "${FUSEKI_DATASET_URL}/update"
 
 # get updated spek
-curl --silent -X GET --header 'Accept: application/ld+json' \
-  'http://localhost:3030/ds?graph=spek'
-
+  curl --silent -G --header 'Accept: application/ld+json' \
+    --data-urlencode "graph=${SPEK_URL}" \
+    "${FUSEKI_DATASET_URL}"
