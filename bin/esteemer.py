67f530be096229f0158e8dd40b28721c642b297f
Ayshwarya Jagadeesan <ayshjag@msla0669.wifi.med.umich.edu>
2022-06-08 12:18:37 -0400

    scoring works for 1 performer 1 measure, 1 performer 10 measures and 1 performer all measures-organizing code is pending

--- bin/esteemer.py
+++ bin/esteemer.py
@@ -9,6 +9,8 @@ from SPARQLWrapper import SPARQLWrapper,XML
 import random
 import pandas as pd
 import sys
+import warnings
+warnings.filterwarnings("ignore")
 def graph_from_file(file):
     g=Graph()
     g.parse(file)
@@ -19,6 +21,7 @@ def graph_from_file(file):
     ?candidate ?p ?o .
     ?candidate obo:RO_0000091 ?disposition .
     ?disposition ?p2 ?o2 .
+    ?candidate slowmo:acceptable_by ?o3 .
     }
     WHERE {
     ?candidate a obo:cpo_0000053 .
@@ -26,6 +29,7 @@ def graph_from_file(file):
     ?candidate obo:RO_0000091 ?disposition .
     ?candidate ?p ?o .
     ?disposition ?p2 ?o2 .
+    ?candidate slowmo:acceptable_by ?o3 .
     }
     ''')
     return qres.graph
@@ -65,49 +69,62 @@ contenders_graph.bind("slowmo",'http://example.com/slowmo#')
 contender_messages_df=to_dataframe(contenders_graph)
 contender_messages_df.reset_index(inplace=True)
 contender_messages_df = contender_messages_df.rename(columns = {'index':'id'})
+#contender_messages_df.to_csv("df_es.csv")
+column_values =['obo:RO_0000091{BNode}[0]','obo:RO_0000091{BNode}[1]','obo:RO_0000091{BNode}[2]','obo:RO_0000091{BNode}[3]','obo:RO_0000091{BNode}[4]','obo:RO_0000091{BNode}[5]','obo:RO_0000091{BNode}[6]','obo:RO_0000091{BNode}[7]','obo:RO_0000091{BNode}[8]','obo:RO_0000091{BNode}[9]','obo:RO_0000091{BNode}[10]']
 
 
 reference_df = contender_messages_df.filter(['id','rdf:type{URIRef}','slowmo:RegardingComparator{BNode}','slowmo:RegardingMeasure{BNode}'], axis=1)
 meaningful_messages_df =contender_messages_df[contender_messages_df['slowmo:AncestorPerformer{Literal}'].notna()]
 reference_df1 =reference_df.dropna()
+
 RegardingComparator=[]
 RegardingMeasure=[]
 disposition=[]
 values=[]
+
 for rowIndex, row in meaningful_messages_df.iterrows(): #iterate over rows
+    b=0
     for columnIndex, value in row.items():
-        if columnIndex == 'obo:RO_0000091{BNode}[0]' or columnIndex == 'obo:RO_0000091{BNode}[1]' or columnIndex == 'obo:RO_0000091{BNode}[2]' or columnIndex == 'obo:RO_0000091{BNode}[3]' or columnIndex == 'obo:RO_0000091{BNode}[4]' or columnIndex == 'obo:RO_0000091{BNode}[5]':
+        if columnIndex in column_values:
             a=reference_df1.loc[reference_df1['id'] == value]
-
             if not a.empty:
-                a.reset_index(drop=True, inplace=True)
-                disposition.append(a['rdf:type{URIRef}'][0])
-                values.append(value)
-                RegardingComparator.append(a['slowmo:RegardingComparator{BNode}'][0])
-                RegardingMeasure.append(a['slowmo:RegardingMeasure{BNode}'][0])
+                if b == 0:
+                    a.reset_index(drop=True, inplace=True)
+                    disposition.append(a['rdf:type{URIRef}'][0])
+                    values.append(value)
+                    RegardingComparator.append(a['slowmo:RegardingComparator{BNode}'][0])
+                    RegardingMeasure.append(a['slowmo:RegardingMeasure{BNode}'][0])
+                    b=b+1
+            #RegardingComparator1 = list(set(RegardingComparator))
 
-
+#print(' '.join(map(str, disposition)))
+#print(' '.join(map(str, RegardingComparator)))
+#print(' '.join(map(str, RegardingMeasure)))
+#print(' '.join(map(str, values)))
+#print(len(values))
+#print(meaningful_messages_df.shape)
 
 meaningful_messages_df["RegardingComparator"] = RegardingComparator
 meaningful_messages_df["RegardingMeasure"] = RegardingMeasure
 meaningful_messages_df["disposition"]=disposition
 meaningful_messages_df["reference_values"]=values
-meaningful_messages_final= meaningful_messages_df.filter(['id','slowmo:AncestorPerformer{Literal}','slowmo:AncestorTemplate{Literal}','disposition','reference_values','rdf:type{URIRef}','psdo:PerformanceSummaryDisplay{Literal}','psdo:PerformanceSummaryTextualEntity{Literal}','RegardingComparator','RegardingMeasure'], axis=1)
+meaningful_messages_final= meaningful_messages_df.filter(['id','slowmo:AncestorPerformer{Literal}','slowmo:AncestorTemplate{Literal}','disposition','reference_values','rdf:type{URIRef}','psdo:PerformanceSummaryDisplay{Literal}','psdo:PerformanceSummaryTextualEntity{Literal}','RegardingComparator','RegardingMeasure','slowmo:acceptable_by{URIRef}[0]','slowmo:acceptable_by{URIRef}[1]'], axis=1)
 
 #random generation of score
 len = meaningful_messages_final.shape[0]
-score = random.sample(range(10, 100), len)
+score = random.sample(range(10,1000), len)
 meaningful_messages_final["score"]=score
+meaningful_messages_final.reset_index(drop=True, inplace=True)
 meaningful_messages_final.to_csv("df_es1.csv")
 
 #max value of score
 column = meaningful_messages_final["score"]
 max_value = column.max()
-print(max_value)
+#print(max_value)
 
-
-
-
-
-
-#contender_messages_df.to_csv("df_es.csv")
+h=meaningful_messages_final['score'].idxmax()
+#print(h)
+message_selected_df = meaningful_messages_final.iloc[h, :]
+message_selected_df =message_selected_df.T
+#print(message_selected_df)
+message_selected_df.to_csv("Selected_Message.csv")
